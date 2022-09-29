f82dff07439921e76e2b87354adb4d6dc99bd7ef
Deep <deep.patel23@outloook.com>
2022-09-29 16:14:16 -0600

    Added the template of the project

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,8 @@
+/* import Contact from './Contact/Contact';
+import Favourites from './Favourites/Favourites';
+import Home from './Home/Home';
+import News from './News/News';
+import Stock from './Stock/Stock';
+import Login from './Login/Login';
+
+export { Contact, Favourites, Home, News, Stock, Login }; */
