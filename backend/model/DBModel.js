b1b94e928e227294eff806cf54d0cbf6ed1140e4
Deep <deep.patel23@outloook.com>
2022-11-08 16:28:36 -0600

    Added model file

--- backend/model/DBModel.js
+++ backend/model/DBModel.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.DATABASE_MONGO);
 client.connect();
-require("dotenv").config();
 
 const db = client.db("Refinance");
 // .collection("Refinance_Stock")
